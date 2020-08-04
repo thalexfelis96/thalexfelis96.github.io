148909939a970cdde8d86cb878fec1ce95c63776
Thales Felix <thales.felix96@gmail.com>
2020-08-04 14:19:30 -0300

    Post lesson

--- src/app/cats/cats.component.ts
+++ src/app/cats/cats.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-cats',
@@ -8,6 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CatsComponent implements OnInit {
   private readonly apiURL = 'https://api.thecatapi.com/v1/images/search';
+  private readonly apiURLFav = 'https://api.thecatapi.com/v1/favourites';
   catsImages: { id: string; url: string }[];
   catsImageId: string[];
 
@@ -25,6 +26,14 @@ export class CatsComponent implements OnInit {
   // }
   showId(i) {
     console.log(this.catsImages[i].id);
+    const opts = {
+      headers: new HttpHeaders({
+        'x-api-key': '0e1a4350-4ac4-47b0-bec2-cb58e9431989',
+      }),
+    };
+    this.http
+      .post(this.apiURLFav, { image_id: `this.catsImageId[i].id` }, opts)
+      .subscribe((dados) => console.log(dados));
   }
 
   ngOnInit(): void {
