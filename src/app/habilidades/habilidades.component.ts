e367b1cf4ee59b656f879d96d8624a4718527d78
Thales Felix <thales.felix96@gmail.com>
2020-08-05 13:15:20 -0300

    Minimal fix

--- src/app/habilidades/habilidades.component.ts
+++ src/app/habilidades/habilidades.component.ts
@@ -10,17 +10,17 @@ export class HabilidadesComponent implements OnInit {
     atirador: {
       image: '../../assets/images/archery.png',
       description:
-        'O atirador a distância é a posição de dano contínuo para a equipe, na dinamica do jogo é o alvo que fica mais forte a medida que o jogo passa',
+        'O atirador a distância é a posição de dano contínuo para a equipe, na dinamica do jogo é o alvo que fica mais forte a medida que o jogo passa.',
     },
     meio: {
       image: '../../assets/images/magic.png',
       description:
-        'A rota do meio é uma posição com funções variadas no jogo, cada campeão exerce uma função diferente de acordo com a necessidade do time',
+        'A rota do meio é uma posição com funções variadas no jogo, cada campeão exerce uma função diferente de acordo com a necessidade do time.',
     },
     selva: {
       image: '../../assets/images/bush.png',
       description:
-        'O caçador é uma posição que seu estilo de jogo sempre muda com tomadas de decisões e tem a função de garantir objetivos neutros para o time',
+        'O caçador é uma posição que seu estilo de jogo sempre muda com tomadas de decisões e tem a função de garantir objetivos neutros para o time.',
     },
   };
 
